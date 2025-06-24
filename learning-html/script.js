ba0b28269f1ed61af8d250ba6df8091351f79506
Nithish1704 <nithishmurugesan46@gmail.com>
2025-06-24 15:56:56 +0530

    modified script.js

--- learning-html/script.js
+++ learning-html/script.js
@@ -1,5 +1,7 @@
 let balance=1000;
-
+const balanceEI=document.getElementById("balance");
+const historyEI=document.getElementById("history");
 function addExpense(){
-
+    const item=document.getElementById("item").value;
+    const amt=parseFloat(document.getElementById("amt"));
 }
