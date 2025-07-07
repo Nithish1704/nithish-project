18a0a4aa2f192cdafa34a7237399bd7a747e5093
Nithish1704 <nithishmurugesan46@gmail.com>
2025-07-07 13:10:27 +0530

    modified

--- learning-html/web page/book.js
+++ learning-html/web page/book.js
@@ -2,11 +2,25 @@ function registar(){
     let selectelement=document.getElementById("pick-up");
     let selectvalue=selectelement.value;
     console.log(selectvalue); 
-    const location=document.getElementById("location");   
-    const day=document.getElementById("day");
-    const rettime=document.getElementById("return-time");
+    const location=document.getElementById("location").value;   
+    const day=document.getElementById("day").value;
+    const rettime=document.getElementById("returntime").value;
+    const loc=localStorage.getItem("details");
+    let data;
+    if(loc){
+        data=JSON.parse(loc);
+        data.push({location:loc,returntime:rettime});
+    }
+    else{
+        data=[{location:loc,returntime:rettime}];
+    }
+    localStorage.setItem("details",JSON.stringify(data));
     if(day==""&&location==""){
         alert("Please enter all the details.");
+        return;
+    }
+    else{
+        alert("Successfully");
         return;
     }
 }
