54f15a5f12e1e5aa3e73f1da5b60dc45a10b496c
Nithish1704 <nithishmurugesan46@gmail.com>
2025-07-08 13:24:09 +0530

    modified

---
+++ learning-html/web page/webpage.js
@@ -0,0 +1,14 @@
+const img=["images.png,image1.png,image2.png"];
+let i=0;
+function showimg(){
+    document.getElementById("slide").src=img[i];
+}
+function next(){
+    i=(i+1)%img.length;
+    console.log(i);
+    showimg();
+}
+function previous(){
+    i=(i-1+img.length)%img.length;
+    showimg();
+}
