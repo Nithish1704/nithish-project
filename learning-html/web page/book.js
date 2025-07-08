485421180c491c4560a931404a73325b7c6e9f75
Nithish1704 <nithishmurugesan46@gmail.com>
2025-07-08 10:34:07 +0530

    modified

--- learning-html/web page/book.js
+++ learning-html/web page/book.js
@@ -1,11 +1,23 @@
 function registar(){
-    let selectelement=document.getElementById("pick-up");
+    let selectelement=document.getElementById("pickup");
     let selectvalue=selectelement.value;
     console.log(selectvalue); 
     const location=document.getElementById("location").value;   
     const day=document.getElementById("days").value;
     const rettime=document.getElementById("returntime").value;
     const loc=localStorage.getItem("details");
+    const priceEl=document.getElementById("price");
+    let price=0;
+    if(selectvalue=="Simmakkal"){
+        price=500;
+    }
+    else if(selectvalue=="Aathikulam"){
+        price=250;
+    }
+    else if(selectvalue=="SuriyaNagar"){
+        price=300;
+    }
+    priceEl.textContent=`Price: ${price}`;
     let data;
     if(loc){
         data=JSON.parse(loc);
