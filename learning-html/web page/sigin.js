bb0dd4fa1f5fbd293293605da17e5150cf8c41df
Nithish1704 <nithishmurugesan46@gmail.com>
2025-07-09 11:58:44 +0530

    modified

--- learning-html/web page/sigin.js
+++ learning-html/web page/sigin.js
@@ -1,5 +1,16 @@
-function submit(){
-    let nameEl=document.getElementById("name").value;
-    console.log(nameEl);   
+function submit() {
+    const nameEl = document.getElementById("name").value;
+    const emailEl=document.getElementById("email").value;
+    const passwordEl=document.getElementById("password").value;
+    console.log(passwordEl);
+    let data;
+    const El=localStorage.getItem("details");
+    if(El){
+        data=JSON.parse(El);
+        data.push({name:nameEl,email:emailEl});
+    }
+    else{
+        data=[{name:nameEl,email:emailEl}];
+    }
+    localStorage.setItem("details",JSON.stringify(data));
 }
-submit();
