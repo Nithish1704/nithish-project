9524d74caa153af6e1c42725a72a5aef99c94e25
Nithish1704 <nithishmurugesan46@gmail.com>
2025-07-11 11:19:39 +0530

    modified

--- learning-html/web page/webpage.js
+++ learning-html/web page/webpage.js
@@ -10,6 +10,10 @@ function changeSlide(n){
 function showSlide(n){
     for(let i=0;i<slides.length;i++){
         slides[i].style.display="none";
+        clearTimeout(slides[i].timeoutId);
+        slides[i].timeoutId = setTimeout(() => {
+            changeSlide(1);
+        }, 2000);
     }
     slides[n].style.display="block";
 }
